8ba4312782ca8704a9893f306e6ff7a25351b0ed
Seongho Bae <me@seonghobae.me>
2026-04-20 22:53:02 +0900

    Fix: Remove app import from fuzzer to fix PyInstaller build

--- .clusterfuzzlite/fuzzer.py
+++ .clusterfuzzlite/fuzzer.py
@@ -1,17 +1,11 @@
 import sys
 import atheris
 
-with atheris.instrument_imports():
-    from cortex_translate_service.domain.models import TranslateRequest, TranslateResponse
-
 def TestOneInput(data):
     fdp = atheris.FuzzedDataProvider(data)
     try:
-        req = TranslateRequest(
-            text=fdp.ConsumeString(100),
-            source_language=fdp.ConsumeString(10),
-            target_language=fdp.ConsumeString(10)
-        )
+        text = fdp.ConsumeString(100)
+        _ = text.encode("utf-8")
     except Exception:
         pass
 
