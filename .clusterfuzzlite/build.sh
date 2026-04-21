68e99855fbba9748f6c603204978d0ee30424e39
Seongho Bae <me@seonghobae.me>
2026-04-21 09:57:05 +0900

    Fix: Pin Docker image, GitHub Actions, and pip to hashes (#64)

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -eu
-pip3 install --ignore-requires-python . atheris==2.3.0
+pip3 install --require-hashes -r .clusterfuzzlite/requirements-fuzz.txt
+pip3 install --ignore-requires-python .
 compile_python_fuzzer .clusterfuzzlite/fuzzer.py
